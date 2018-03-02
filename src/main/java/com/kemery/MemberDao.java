26178f9b2d63ddce1db13848bb47ba0fc58446f2
kmne6 <kmne6@10.37.35.18>
2018-03-01 18:35:25 -0600

    Add findByStatus method

--- src/main/java/com/kemery/MemberDao.java
+++ src/main/java/com/kemery/MemberDao.java
@@ -1,6 +1,9 @@
 package com.kemery;
 
+import java.util.List;
+
 public interface MemberDao {
 
 	public Member find(String memid);
+	public List<Member> findByStatus(String status);
 }
--- src/main/java/com/kemery/MemberDaoJdbcImpl.java
+++ src/main/java/com/kemery/MemberDaoJdbcImpl.java
@@ -2,17 +2,22 @@ package com.kemery;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class MemberDaoJdbcImpl implements MemberDao {
 
 	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		
 		this.jdbcTemplate = jdbcTemplate;
+		namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
 	}
 	
 	public Member find(String memid) {
@@ -31,6 +36,25 @@ public class MemberDaoJdbcImpl implements MemberDao {
 				return m;
 			}
 		}, memid);
+	}
+	
+	public List<Member> findByStatus(String stat) {
+		
+		String sql = "SELECT memid, lastname, firstname, middlename, status, memdt, password FROM tblmember WHERE status = :status";
+		return namedParameterJdbcTemplate.query(sql, Collections.singletonMap("status", stat),
+				new RowMapper<Member>() {
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member m = new Member();
+				m.setMemid(rs.getString("memid"));
+				m.setLastnm(rs.getString("lastname"));
+				m.setFirstnm(rs.getString("firstname"));
+				m.setMiddlenm(rs.getString("middlename"));
+				m.setStatus(rs.getString("status"));
+				m.setMemdt(rs.getString("memdt"));
+				m.setPassword(rs.getInt("password"));
+				return m;
+			}
+		});
 	}
 	
 }
